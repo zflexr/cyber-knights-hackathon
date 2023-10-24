b14a88a1a68a8f5720ee9793f2d3f661ddb32ebe
Zflexr <77554745+zflexr@users.noreply.github.com>
2023-10-24 15:21:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/base', baseouter);
+app.use('/verify', baseouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
