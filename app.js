fa1e8817641b65cabac0e7683d9ef95ba412e5b9
Zflexr <77554745+zflexr@users.noreply.github.com>
2023-10-26 20:53:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/verify', baseouter);
+app.use('/base', baseouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
